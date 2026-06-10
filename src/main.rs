fb3ac3cbd89aace27ceb993090b7f472b268f2a9
Screensavers Developer <dev@local76.org>
2026-06-10 10:29:09 -1000

    Refactor: migrate local runner to centralized library::screensaver_runner

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-mod runner;
 mod bounce;
 
 fn main() {
     let effect = bounce::Bounce::new();
-    runner::run_main(effect, "bounce");
+    library::screensaver_runner::run_main(effect, "bounce");
 }
